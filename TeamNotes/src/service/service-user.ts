254fe54516a912d374cbf632767879deac453adf
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-09-12 13:53:16 +0300

    change App.tsx

--- TeamNotes/src/service/service-user.ts
+++ TeamNotes/src/service/service-user.ts
@@ -7,3 +7,28 @@ export const registerUser = (email: string, password: string): Promise<UserCrede
   return createUserWithEmailAndPassword(auth, email, password);
 };
 
+export const createUserProfile = (
+  uid: string,
+  username: string,
+  email: string,
+  password: string,
+  phoneNumber: string,
+  notifications: any,
+  status: string,
+  role: string  = "user",
+) => {
+  const readableDate = new Date().toLocaleString(); 
+
+  return set(ref(db, `users/${uid}`), {
+    uid,
+    username,
+    email,
+    password,
+    phoneNumber,
+    userPhotoURL: "",
+    notifications,
+    status,
+    role,
+    createdOnReadable: readableDate,
+  });
+};
