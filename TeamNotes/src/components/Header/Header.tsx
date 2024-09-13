a61de400b364a408dd4e42e58e7c91a8a5744bce
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-09-13 19:29:41 +0300

    add Header component

---
+++ TeamNotes/src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import firebaseConfig from "../../service/firebase-config";
+import { useAppContext } from "../../AppContext";
+
+const Header=()=>{
+    const navigate = useNavigate();
+    const { user, userData, setContext } = useAppContext();
+
+    const logout = async () => {
+        await logoutUser();
+        setContext({ user: null, userData: null });
+        navigate('/login');
+      }
+      const profile = () => {
+        navigate(`/profile/${userData?.username}`);
+      };
+
+      return(
+            <div>
+                <button onClick={logout}>Logout</button>
+                <button onClick={profile}>Profile</button>
+            </div>
+      )
+}
+export default Header;
