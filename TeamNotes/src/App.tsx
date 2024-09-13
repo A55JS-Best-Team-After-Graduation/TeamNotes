d53d9cb17ef503befc938d4b9270db592183daf7
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-09-13 20:43:08 +0300

    update import path

--- TeamNotes/src/App.tsx
+++ TeamNotes/src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useContext } from 'react'
 import { BrowserRouter, Routes, Route, useParams, Navigate } from "react-router-dom";
 import './App.css'
 import Home from './views/Home/Home';
-import Register from './views/Register';
+import Register from './views/Register/Register';
 import Auth from './hoc/Auth';
 import Login from './views/Login/Login';
 import Calendar from './views/Calendar/Calendar';
