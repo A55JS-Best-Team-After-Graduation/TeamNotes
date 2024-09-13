630f82141c2e1a312d2680bb43ca38d1a6039cf2
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-09-13 20:42:25 +0300

    add Login and Calendar routes to App component

--- TeamNotes/src/App.tsx
+++ TeamNotes/src/App.tsx
@@ -4,19 +4,26 @@ import './App.css'
 import Home from './views/Home/Home';
 import Register from './views/Register';
 import Auth from './hoc/Auth';
+import Login from './views/Login/Login';
+import Calendar from './views/Calendar/Calendar';
+
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+    
         <Routes>
           <Route path="/" element={<Navigate to="/home" replace />} />
           {/* <Route path="/home" element={<Auth> <Home /> </Auth>} /> */}
           <Route path="/home" element={<Home />} />
           <Route path="/register" element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/calendar' element= {<Calendar />} />
           <Route path="*" element={<div>404 - Page Not Found</div>} />
         </Routes>
+       
       </BrowserRouter>
     </>
   )
